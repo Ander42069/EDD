6af8f9b95a89b285aaa806b1f6ab8279ea948d16
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2021-10-22 20:24:00 -0600

    Add files via upload

---
+++ EVA1_17_EJEMPLO_MATRIZ/src/eva1_17_ejemplo_matriz/EVA1_17_EJEMPLO_MATRIZ.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_17_ejemplo_matriz;
+
+/**
+ *
+ * @author Perla
+ */
+public class EVA1_17_EJEMPLO_MATRIZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] aiMatriz = new int[3][];
+        //FILAS        COLUMNAS
+        aiMatriz[0] = new int[2];//FILA 0 ARREGLO DE 2 COLUMNAS
+        aiMatriz[1] = new int[10];//FILA 1 ARREGLO DE 10 COLUMNAS
+        aiMatriz[2] = new int[5];//FILA 2 ARREGLO DE 5 COLUMNAS
+        //¿Cuantos enteros puedo almacenar?
+    }
+    
+}
