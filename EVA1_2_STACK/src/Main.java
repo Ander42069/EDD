1bec8294792b74805d075a8c7773a49e8b1fe173
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2021-10-22 20:18:57 -0600

    Add files via upload

---
+++ EVA1_2_STACK/src/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Perla
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("INICIAN MAIN");
+        A();
+        System.out.println("TERMINA MAIN");
+    }
+    
+    public static void A(){
+        System.out.println("INICIAN A");
+        B();
+        System.out.println("TERMINA A");
+    }
+    
+    public static void B(){
+        System.out.println("INICIA B");
+        int i = 0;
+        double d = 0.0;
+        System.out.println("TERMINA B");
+    }
+}
