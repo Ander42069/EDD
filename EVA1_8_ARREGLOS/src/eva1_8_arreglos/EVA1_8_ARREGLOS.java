c3f80b163074da7de4c56aa3fab1dc22e515a907
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2021-10-22 20:21:18 -0600

    Add files via upload

---
+++ EVA1_8_ARREGLOS/src/eva1_8_arreglos/EVA1_8_ARREGLOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_arreglos;
+
+/**
+ *
+ * @author Perla
+ */
+public class EVA1_8_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] aiMiArreglo;//declaracion, el arreglo no existe todavia
+        //ARREGLOS SON OBJETOS
+        aiMiArreglo = new int[10];//asignando memoria para 10 objetos
+        int[] aiOtroArreglo = new int[50];//declaramos y creamos el arreglo
+        
+        double[]  adArre1, adArre2, adArre3;
+        
+        double adOtroArre[], dVariable, dOtraVar; //solo un arreglo, dos variables de tipo double
+        
+    }
+    
+}
