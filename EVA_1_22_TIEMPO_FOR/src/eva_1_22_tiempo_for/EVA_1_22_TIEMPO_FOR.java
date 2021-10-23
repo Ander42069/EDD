35e8bf7f695e9af22a90535faf20957f6a74aec9
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2021-10-22 20:25:10 -0600

    Add files via upload

---
+++ EVA_1_22_TIEMPO_FOR/src/eva_1_22_tiempo_for/EVA_1_22_TIEMPO_FOR.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_22_tiempo_for;
+
+/**
+ *
+ * @author Perla
+ */
+public class EVA_1_22_TIEMPO_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] aiArray = new int[1000000];//4 bytes*1000000 = 4 MEGAS
+        for (int i = 0; i < aiArray.length; i++){
+            aiArray[i] = 100;
+        }
+    }
+    
+}
