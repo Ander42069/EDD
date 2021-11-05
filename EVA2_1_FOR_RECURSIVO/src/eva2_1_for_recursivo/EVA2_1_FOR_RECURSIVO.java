52b27be7c360c6c3aff2e91db2dcba14fc86d5a9
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2021-11-05 15:50:13 -0700

    Add files via upload

---
+++ EVA2_1_FOR_RECURSIVO/src/eva2_1_for_recursivo/EVA2_1_FOR_RECURSIVO.java
@@ -0,0 +1,21 @@
+package eva2_1_for_recursivo;
+
+/**
+ *
+ * @author Perla
+ */
+public class EVA2_1_FOR_RECURSIVO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        recursivo();
+    }
+    
+    public static void recursivo(){
+    System.out.println("HOLA MUNDO!!");
+    recursivo();
+    }
+}
