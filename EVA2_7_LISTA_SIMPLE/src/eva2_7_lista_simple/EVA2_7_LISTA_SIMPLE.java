4ec159d27677cf750ce14de7939cda26bf29fecf
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2021-11-26 13:04:10 -0700

    Add files via upload

---
+++ EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/EVA2_7_LISTA_SIMPLE.java
@@ -0,0 +1,58 @@
+package eva2_7_lista_simple;
+
+/**
+ *
+ * @author Perla
+ */
+public class EVA2_7_LISTA_SIMPLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //CREAR UNA LISTA
+        Lista miLista = new Lista();//creo una lista
+       
+        /*int[] miArreglo = new int[1000000];
+        for (int i = 0; i < miArreglo.length; i++){
+            miArreglo[i] = (int)(Math.random() * 1000);
+        }
+        for (int i = 0; i < 1000000; i++){
+            miLista.add(new Nodo(1));
+        }*/
+        
+        miLista.add(100);//0
+        miLista.add(200);//1
+        miLista.add(300);//2
+        miLista.add(400);//3
+        miLista.add(500);//4
+        System.out.println("Conteo: " + miLista.length());
+        miLista.printList();
+        
+        System.out.println("\nDespues de insertar ");
+        miLista.insertAt(4, 9999);
+        System.out.println("Conteo: " + miLista.length());
+        miLista.printList();
+        
+        System.out.println("\nDepues de borrar ");
+        miLista.deleteAt(5);
+        miLista.printList();
+        
+        System.out.println("Conteo: " + miLista.length());
+        miLista.add(7777);
+        System.out.println("\nDespues de agregar");
+        miLista.printList();
+        
+        System.out.println("\nConteo: " + miLista.length());
+        
+        for(int i = miLista.length() - 1;i >= 0;i--){
+            try{
+            System.out.print(miLista.get(i) + " - ");
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+            }
+   }
+    
+}
